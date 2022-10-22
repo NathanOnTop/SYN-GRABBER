8bcf2b0bd7e53b81e720367f7213ff4605bb63f3
Termed$#0001 <104595114+NathanOnTop@users.noreply.github.com>
2022-10-22 15:48:36 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,3 +5,8 @@ pip install time
 pip install json
 pip install requests
 pip install os
+pip install pypiwin32
+pip install Pillow
+pip install pycryptodomex
+pip install psutil
+pip install discordwebhook
